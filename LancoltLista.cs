bb6bf94545e99d8fc35b1abb3a2f270b5075073f
Gocze Norbert <gnorbi951@gmail.com>
2021-04-29 14:26:13 +0200

    Add Node internal class

--- LancoltLista.cs
+++ LancoltLista.cs
@@ -6,6 +6,15 @@ namespace SpecLinkedList
 {
     class LancoltLista
     {
-
+        internal class Node
+        {
+            internal int data;
+            internal Node next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
     }
 }
